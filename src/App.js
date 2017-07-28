f21d0cd109428edbf6d63c8a7b871df46fdfa121
AscaL <emanueleorfanelli@gmail.com>
2017-07-28 17:27:41 +0200

    working routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Helmet from 'react-helmet';
+import { Navbar, NavItem, Nav } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import About from './components/About';
+import Home from './components/Home';
+import Songs from './components/Songs';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <Helmet title="DrDoctor DJ Page" />
+        <div>
+          <Router>
+            <div>
+              <div>
+                <Navbar fluid>
+                  <Navbar.Header>
+                    <Navbar.Brand>
+                      <a href="#">DrDoctor DJ Page</a>
+                    </Navbar.Brand>
+                  </Navbar.Header>
+                  <Nav>
+                    <NavItem>
+                      <Link to="/home">Home</Link>
+                    </NavItem>
+                    <NavItem>
+                      <Link to="/about">About</Link>
+                    </NavItem>
+                    <NavItem>
+                      <Link to="/songs">Songs</Link>
+                    </NavItem>
+                  </Nav>
+                </Navbar>
+              </div>
+              <Route path="/home" component={Home} />
+              <Route path="/songs" component={Songs} />
+              <Route path="/about" component={About} />
+            </div>
+          </Router>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from 'react-snapshot';
 import App from './App';
+import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
 registerServiceWorker();
